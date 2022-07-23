2c3b74eb3cf3c3cf33a26e094c39265b8658901f
SomvanshiGit <91210623+SomvanshiGit@users.noreply.github.com>
2022-07-23 18:21:17 +0530

    Commit 23-07-22 time 6:20pm

---
+++ java_learn/src/Inheritance/SingleLevel.java
@@ -0,0 +1,38 @@
+package Inheritance;
+
+class A
+{
+	int a;
+	void display()
+	{
+		System.out.println(a);
+	}
+}
+class B extends A   // B is child class and A is parent class and to acess A class stuff use extends
+{
+	int b;
+	void print()
+	{
+		System.out.println(b);
+	}
+	
+}
+
+public class SingleLevel {
+
+	
+	public static void main(String[] args) {
+		A aobj=new A();
+		aobj.a=100;
+		aobj.display();
+		
+		//Class B can access the Class A variable and method becoz B is child class of parent A class 
+		B bobj=new B();
+		bobj.a=100;
+		bobj.b=200;
+		bobj.display();
+		bobj.print();		
+
+	}
+
+}
