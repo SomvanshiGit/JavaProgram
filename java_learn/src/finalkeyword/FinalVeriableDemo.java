45bf6bfbee7d2c12fd521662b577203167ab7874
SomvanshiGit <91210623+SomvanshiGit@users.noreply.github.com>
2022-09-26 21:46:55 +0530

    final keyword

--- java_learn/src/finalkeyword/FinalVeriableDemo.java
+++ java_learn/src/finalkeyword/FinalVeriableDemo.java
@@ -3,7 +3,8 @@ package finalkeyword;
 public class FinalVeriableDemo {
 	
 	final int speed=400;  // final variable so it can not change the value 
-
+	
+	
 	public static void main(String[] args) {
 		FinalVeriableDemo fm= new FinalVeriableDemo();
 		//fm.speed=100; // we can not assign / change value of final variable 
